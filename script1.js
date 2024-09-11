c97ba4de7f9f4edc32ba53770d4736d9dbc46ee9
samuelfas26 <samuelfas26@gmail.com>
2024-09-11 16:13:37 -0400

    Update script1.js

--- script1.js
+++ script1.js
@@ -1,51 +1,16 @@
 document.addEventListener('DOMContentLoaded', function () {
-    const urlParams = new URLSearchParams(window.location.search);
-    const selectedMaterials = JSON.parse(urlParams.get('materials')) || [];
+    const materials = [
+        'Viatura', 'Fuzil 7,62', 'Fuzil 5,56', 'Espingarda Cal 12', 'Pistola Beretta', 'Pistola Imbel',
+        'Carregador Sobressalente Fuzil', 'Carregador Sobressalente Pistola', 'Munição 7,62', 'Munição 9mm',
+        'Bandoleira', 'Colete Tático', 'Capacete Balístico', 'Placa Balística', 'Spray de Pimenta Pequeno',
+        'Granada Lacrimogênea', 'Granada Luz e Som', 'Kit Antitumulto', 'Escudo OCD', 'Tonfa', 'Rádio',
+        'Bateria Sobressalente', 'Barraca Iglu', 'Rede Operacional', 'Manta Térmica', 'Mochila', 'Marmita',
+        'Poncho', 'Saco de Dormir', 'Mochila T10', 'Óculos Tático', 'Lanterna', 'Camelback'
+    ];
 
-    const materialsBody = document.getElementById('material-selection');
+    const materialSelection = document.getElementById('material-selection');
 
-    // Adiciona os materiais à página
-const materials = [
-    'Viatura', 
-    'Fuzil 7,62', 
-    'Fuzil 5,56', 
-    'Espingarda Cal 12', 
-    'Pistola Beretta', 
-    'Pistola Imbel', 
-    'Carregador Sobressalente Fuzil', 
-    'Carregador Sobressalente Pistola', 
-    'Munição 7,62', 
-    'Munição 9mm', 
-    'Munição Cal 12 Elastômero', 
-    'Munição Cal 12 Explosiva', 
-    'Bandoleira', 
-    'Colete Tático', 
-    'Capacete Balístico', 
-    'Placa Balística', 
-    'Spray de Pimenta Pequeno', 
-    'Granada Lacrimogênea', 
-    'Granada Luz e Som', 
-    'Kit Antitumulto', 
-    'Escudo OCD', 
-    'Tonfa', 
-    'Rádio', 
-    'Bateria Sobressalente', 
-    'Barraca Iglu', 
-    'Rede Operacional', 
-    'Manta Térmica', 
-    'Mochila', 
-    'Marmita', 
-    'Poncho', 
-    'Saco de Dormir', 
-    'Saco de Campanha', 
-    'Mochila T10', 
-    'Óculos Tático', 
-    'Lanterna', 
-    'Bornal', 
-    'Camelback'
-];
-
-
+    // Exibe os materiais na página
     materials.forEach(material => {
         const div = document.createElement('div');
         div.className = 'material-item';
@@ -54,7 +19,7 @@ const materials = [
         div.addEventListener('click', () => {
             div.classList.toggle('selected');
         });
-        materialsBody.appendChild(div);
+        materialSelection.appendChild(div);
     });
 
     document.getElementById('next-step').addEventListener('click', () => {
@@ -63,6 +28,8 @@ const materials = [
             alert('Nenhum material selecionado.');
             return;
         }
+
+        // Salva os materiais selecionados na URL e redireciona para a próxima página
         const params = new URLSearchParams();
         params.append('materials', JSON.stringify(selectedMaterials));
         window.location.href = `2.html?${params.toString()}`;
